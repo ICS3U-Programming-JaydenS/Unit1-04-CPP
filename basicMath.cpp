f482ff656a0f250cf51e85296c2c953dac6a52a3
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-19 02:58:55 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Jayden Smith All rights reserved.
+// Created by: Jayden Smith
+// Date: February 18, 2025
+// Preforms basic math.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "22+3=" << (22 + 3) << std::endl;
+    std::cout << "22-8=" << (22 - 8) << std::endl;
+    std::cout << "0.5/7=" << (0.5 / 7) << std::endl;
+    std::cout << "22*3=" << (22 * 3) << std::endl;
+    std::cout << "2^3=" << (pow(2, 3)) << std::endl;
+    std::cout << " √16=" << (sqrt(16)) << std::endl;
+}
